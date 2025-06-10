2b9813f28b0b2b3f5be5fd97340678b3e7211949
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-06-10 12:27:55 +0100

    Add initial project files including .gitignore, go.mod, go.sum, and main.go with bot logic

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+var numericKeyboard = tgbotapi.NewReplyKeyboard(
+    tgbotapi.NewKeyboardButtonRow(
+        tgbotapi.NewKeyboardButton("1"),
+        tgbotapi.NewKeyboardButton("2"),
+        tgbotapi.NewKeyboardButton("3"),
+    ),
+    tgbotapi.NewKeyboardButtonRow(
+        tgbotapi.NewKeyboardButton("4"),
+        tgbotapi.NewKeyboardButton("5"),
+        tgbotapi.NewKeyboardButton("6"),
+    ),
+)
+
+func main() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get the bot token from the environment variable
+	token := os.Getenv("TELEGRAM_APITOKEN")
+	if token == "" {
+		log.Fatal("TELEGRAM_APITOKEN environment variable is not set")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Fatalf("Error creating bot: %v", err)
+	}
+
+	bot.Debug = true
+
+	fmt.Printf("Authorized on account %s\n", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+        if update.Message == nil { // ignore any non-Message updates
+            continue
+        }
+msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+
+        // Extract the command from the Message.
+switch update.Message.Text {
+        case "Open":
+            msg.ReplyMarkup = numericKeyboard
+        case "Close":
+            msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+        }
+
+        if _, err := bot.Send(msg); err != nil {
+            log.Panic(err)
+        }
+    }
+
+	// Add your bot logic here
+}
